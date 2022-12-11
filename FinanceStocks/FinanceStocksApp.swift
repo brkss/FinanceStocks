a448770d785fbd4041d34b1d7df3186e98a607ea
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-11 19:00:14 +0100

    Initial Commit

---
+++ FinanceStocks/FinanceStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceStocksApp.swift
+//  FinanceStocks
+//
+//  Created by Brahim Berkasse on 11/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FinanceStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
